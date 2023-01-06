7de55974881fb1b3d12cb176a45af70b6c5baab3
Derick Vitancol <derickvitancol@gmail.com>
2023-01-06 22:44:27 +0800

    First Commit, initialized the repository

---
+++ src/main/java/PrintMonitor.java
@@ -0,0 +1,10 @@
+public class PrintMonitor {
+    public static void main(String[] args)
+    {
+        //Make the app run in the background
+        //check the log file
+        //make sure the log file is updated
+        //send a print command if necessary
+
+    }
+}
